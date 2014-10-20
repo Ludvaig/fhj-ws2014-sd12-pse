9ef04acdc08d1892598c0bcc9c4c022142760cf7
joerg86@gmail.com <joerg86@gmail.com@b4c154ee-1d16-9667-695d-569dda043578>
2014-10-20 22:15:47 +0000

    User Entity Test added; swd12 folder removed swd folder added

---
+++ trunk/src/test/java/at/fhj/swd/model/entity/UserTest.java
@@ -0,0 +1,87 @@
+package at.fhj.swd.model.entity;
+
+import at.fhj.swd.model.entity.User;
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+
+	public void testEqual_success() {
+
+		User user = new User();
+
+	   // Tests
+	   assertEquals(user, user);
+	 } 
+	
+	public void testEqualNull_fail() {
+	   // Tests
+	   assertEquals(false, new User().equals(null));
+	 } 
+	
+	public void testEqualWrongClass_fail() {
+	   // Tests
+	   assertEquals(false, new User().equals(new String()));
+	} 
+	
+	public void testEqualId_success() {
+		User userOne = new User();
+		User userTwo = new User();
+		
+		userOne.setId((long) 1);
+		userTwo.setId((long) 1);
+		
+	   // Tests
+	   assertEquals(true, userOne.equals(userTwo));
+	} 
+	
+	public void testEqualWrongId_fail() {
+		User userOne = new User();
+		User userTwo = new User();
+		
+		userOne.setId((long) 1);
+		userTwo.setId((long) 2);
+		
+	   // Tests
+	   assertEquals(false, userOne.equals(userTwo));
+	} 
+	
+	public void testEqualullId_fail() {
+		User userOne = new User();
+		User userTwo = new User();
+		
+		
+	   // Tests
+	   assertEquals(false, userOne.equals(userTwo));
+	} 
+	
+	public void testHashCodeSameId_succes() {
+		User userOne = new User();
+		User userTwo = new User();
+		
+		userOne.setId((long) 1);
+		userTwo.setId((long) 1);
+		
+	   // Tests
+	   assertEquals(userOne.hashCode(), userTwo.hashCode());
+	} 
+	
+	public void testHashCodeDiffrentId_fail() {
+		User userOne = new User();
+		User userTwo = new User();
+		
+		userOne.setId((long) 1);
+		userTwo.setId((long) 2);
+		
+	   // Tests
+		assertFalse(userOne.hashCode() == userTwo.hashCode());
+	} 
+	
+	public void testHashCodeNullId_fail() {
+		User userOne = new User();
+		User userTwo = new User();
+			
+	   // Tests
+		assertFalse(userOne.hashCode() == userTwo.hashCode());
+	} 
+		
+}
