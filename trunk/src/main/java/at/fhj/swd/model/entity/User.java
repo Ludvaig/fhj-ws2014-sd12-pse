25abe73679f8680ff76a4d0349cd13c41be3f40e
joerg86@gmail.com <joerg86@gmail.com@b4c154ee-1d16-9667-695d-569dda043578>
2014-10-20 22:03:12 +0000

    User Enitiy added

---
+++ trunk/src/main/java/at/fhj/swd/model/entity/User.java
@@ -0,0 +1,83 @@
+package at.fhj.swd.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * user class from an central storage
+ * (for example active directory)
+ * user with the same id are always equal
+ * 
+ * @author Jörg Huber
+ */
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id = null;
+
+	@Column
+	private String username;
+
+	@Column
+	private String password;
+
+	@Column
+	private String email;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object that) {
+		if (this == that) {
+			return true;
+		}
+		if (that == null) {
+			return false;
+		}
+		if (getClass() != that.getClass()) {
+			return false;
+		}
+		if (id != null) {
+			return id.equals(((User) that).id);
+		}
+		return super.equals(that);
+	}
+
+	@Override
+	public int hashCode() {
+		if (id != null) {
+			return id.hashCode();
+		}
+		return super.hashCode();
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(final String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+}
