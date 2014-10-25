144db271236359497948fe239dc5dee0e30e499b
joerg86@gmail.com <joerg86@gmail.com@b4c154ee-1d16-9667-695d-569dda043578>
2014-10-25 11:16:54 +0000

    user Service and dao added (not yet finished)

---
+++ trunk/src/main/java/at/fhj/swd/model/service/UserService.java
@@ -0,0 +1,28 @@
+package at.fhj.swd.model.service;
+
+import javax.ejb.Stateful;
+
+import at.fhj.swd.model.entity.User;
+
+/**
+ * service to load information
+ * about user objects
+ * 
+ * @author Jörg Huber
+ */
+public interface UserService {
+	
+	/**
+	 * register user with user name and password
+	 * on success this method will return true
+	 * */
+	void registerUser(String userName,String password);
+	
+	/**
+	 * get the registered user
+	 * if the user has not registered yet
+	 * the method will return null
+	 * */
+	User getRegisteredUser();
+
+}
