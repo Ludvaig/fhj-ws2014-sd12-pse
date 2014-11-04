14cf9ad668acfbbc7ac8846101bb89ad5fc3d964
Thomas.Ascher.SWD12@gmail.com <Thomas.Ascher.SWD12@gmail.com@b4c154ee-1d16-9667-695d-569dda043578>
2014-11-04 19:08:48 +0000

    added more method for doclib upload, test for upload

--- trunk/src/main/java/at/fhj/swd/model/service/DocumentService.java
+++ trunk/src/main/java/at/fhj/swd/model/service/DocumentService.java
@@ -1,5 +1,7 @@
 package at.fhj.swd.model.service;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import at.fhj.swd.model.entity.Document;
@@ -11,4 +13,20 @@ import at.fhj.swd.model.entity.Document;
  * */
 public interface DocumentService {
 	public List<Document> getGlobalDocuments();
+	
+	public void uploadGlobalDocument(InputStream source, final String name) throws IOException;
+
+	public void deleteGlobalDocument(final String name);
+	
+	public List<Document> getCommunityDocuments(final String community);
+
+	public void uploadCommunityDocument(final String community, InputStream source, final String name) throws IOException;
+
+	public void deleteCommunityDocument(final String community, final String name);
+	
+	public List<Document> getUserDocuments(final String user);
+	
+	public void uploadUserDocument(final String user, InputStream source, final String name) throws IOException;	
+
+	public void deleteUserDocument(final String user, final String name);
 }
--- trunk/src/main/java/at/fhj/swd/model/service/Impl/DocumentServiceImpl.java
+++ trunk/src/main/java/at/fhj/swd/model/service/Impl/DocumentServiceImpl.java
@@ -1,6 +1,11 @@
 package at.fhj.swd.model.service.Impl;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,17 +24,63 @@ import at.fhj.swd.model.service.DocumentService;
 @ApplicationScoped
 public class DocumentServiceImpl implements DocumentService {
 
-	static String documentHome = "/home/student";
+	private static String documentHome = "/tmp/doclib";
 	
-	static String globalContext = "global";
+	private static String globalContext = "global";
+	
+	private static String communityContext = "community";
+	
+	private static String userContext = "user";
 	
 	@Override
 	public List<Document> getGlobalDocuments() {
-		return this.getDocuments(globalContext);
+		return this.getDocuments(this.getGlobalPath());
+	}
+
+	@Override
+	public void uploadGlobalDocument(InputStream source, String name) throws IOException {
+		this.uploadDocument(source, name, this.getGlobalPath());
 	}
 	
-	private List<Document> getDocuments(final String pathExtension) {
-		final File folder = new File(documentHome, pathExtension);
+	@Override
+	public void deleteGlobalDocument(String name) {
+		this.deleteDocument(name, this.getGlobalPath());
+	}
+
+	@Override
+	public List<Document> getCommunityDocuments(String community) {
+		return this.getDocuments(this.getCommunityPath(community));
+	}
+
+	@Override
+	public void uploadCommunityDocument(final String community, InputStream source, String name) throws IOException {
+		this.uploadDocument(source, name, this.getCommunityPath(community));
+	}
+
+	@Override
+	public void deleteCommunityDocument(String community, String name) {
+		this.deleteDocument(name, this.getCommunityPath(community));
+	}
+
+	@Override
+	public List<Document> getUserDocuments(String user) {
+		return this.getDocuments(this.getUserPath(user));		
+	}
+
+	@Override
+	public void uploadUserDocument(final String user, InputStream source, String name) throws IOException {
+		this.uploadDocument(source, name, this.getUserPath(user));
+	}
+
+	@Override
+	public void deleteUserDocument(String user, String name) {
+		this.deleteDocument(name, this.getUserPath(user));	
+	}	
+	
+	private List<Document> getDocuments(final String path) {
+		this.ensurePath(path);
+		
+		final File folder = new File(path);
 		List<Document> docs = new ArrayList<>();
 		
 		for (final File fileEntry : folder.listFiles()) {
@@ -41,5 +92,43 @@ public class DocumentServiceImpl implements DocumentService {
 		}
 		
 		return docs;
+	}
+
+	private String getGlobalPath() {
+		return this.concatPath(documentHome, globalContext);
+	}	
+
+	private String getCommunityPath(final String community) {
+		return this.concatPath(this.concatPath(documentHome, communityContext), community);
+	}	
+	
+	private String getUserPath(final String user) {
+		return this.concatPath(this.concatPath(documentHome, userContext), user);
+	}
+	
+	private String concatPath(final String prefix, final String postfix) {
+		return new File(prefix, postfix).toString();
+	}
+	
+	private void ensurePath(final String path) {
+		new File(path).mkdirs();
+	}
+
+	private void deleteDocument(final String name, final String path) {
+		this.ensurePath(path);
+	}	
+	
+	private void uploadDocument(InputStream source, final String name, final String path) throws IOException {
+		this.ensurePath(path);
+		
+		OutputStream out = new FileOutputStream(new File(path, name));
+		int read = 0;
+		byte[] bytes = new byte[1024]; 
+		while ((read = source.read(bytes)) != -1) {
+			out.write(bytes, 0, read);
+		}
+		source.close();
+		out.flush();
+		out.close();
 	}
 }
