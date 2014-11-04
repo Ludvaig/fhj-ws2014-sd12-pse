25b62e0e420435805fe65a753fcc5de0dd792c4b
florian.dulzky <florian.dulzky@gmail.com>
2014-11-04 20:44:00 +0000

    Implemented community search

--- src/main/java/at/fhj/swd/model/data/impl/CommunityDaoImpl.java
+++ src/main/java/at/fhj/swd/model/data/impl/CommunityDaoImpl.java
@@ -18,12 +18,18 @@ public class CommunityDaoImpl implements CommunityDao {
 	public List<Community> getSubscribedCommunitiesForSearchTextOfCurrentUser(
 			String searchFieldText, User user) {
 
+		// Load limited Community-Entry from Database (Pagination @see: LazyCommunityImpl)
 		List<Community> communities = em
 				.createQuery(
-						"select community_id from user_has_community uhc inner join community com on uhc.community_id = com.id where uhc.user_id=:user_id and com.name like '%:com_name%'",
-						Community.class).setParameter("user_id", user.getId())
-				.setParameter("com_name", searchFieldText).getResultList();
-
+						"SELECT com FROM Community com"
+						+ " INNER JOIN com.users u"
+						+ " WHERE u.id = :user_id "
+						+ " AND upper(com.name) LIKE upper(:com_name) ",
+						Community.class)
+							.setParameter("user_id", user.getId())
+							.setParameter("com_name", "%" + searchFieldText + "%")
+							.getResultList();
+		
 		return communities;
 	}
 
