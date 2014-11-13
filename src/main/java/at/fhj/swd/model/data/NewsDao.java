6dcbb25970e881f6ebe4be79ad646e1eee61c5b7
etops77 <etops77@gmail.com>
2014-11-13 20:09:28 +0000

    added NewsDao and NewsDaoImpl

---
+++ src/main/java/at/fhj/swd/model/data/NewsDao.java
@@ -0,0 +1,11 @@
+package at.fhj.swd.model.data;
+
+import java.util.List;
+import at.fhj.swd.model.entity.News;
+
+public interface NewsDao {
+
+	public List<News> getAllVisibleNews();
+	public News getNewsById(String id);
+
+}
