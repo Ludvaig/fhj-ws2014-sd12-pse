162c41b134f70f57d213f4e1514e971b4711321e
tschuliiia <tschuliiia@gmail.com>
2014-11-14 17:06:14 +0000

    newsservice impl, test refactoring, newsdao änderung

--- src/main/java/at/fhj/swd/model/data/UserDAO.java
+++ src/main/java/at/fhj/swd/model/data/UserDAO.java
@@ -2,46 +2,21 @@ package at.fhj.swd.model.data;
 
 import at.fhj.swd.model.entity.User;
 
-/**
- * Data Access Object for the User entity
- * 
- * @author Jörg Huber
- * */
 public interface UserDAO {
 
-	/**
-	 * prove if the given combination of user name
-	 * and password is valid
-	 * */
-	User proveUserPassswordCombination(String userName,String password);
 	
-	/**
-	 * persist a User to database
-	 **/
-	void persist(User user);
+		User proveUserPassswordCombination(String username, String password);
 	
-	/**
-	 *load a User by his user name
-	 */
-	User loadUserByName(String username);
-	
-	
-	/**
-	 *load a User by user id
-	 */
-	User loadUserById(long id);
-	
-	/**
-	 *load a User by his token
-	 */
-	User loadUserByToken(String token);
-	
-	/**
-	 * updates the given user
-	 * @param user
-	 * @return
-	 */
-	User updateUser(User user);
-	
+		void persist(User user);
+		User loadUserByName(String username);
+		
+		
+		User loadUserById(long id); 
+		User loadUserByToken(String token); 
+		User updateUser(User user);
+		
 
-}
+
+	}
+
+
--- src/main/java/at/fhj/swd/model/service/Impl/NewsServiceImpl.java
+++ src/main/java/at/fhj/swd/model/service/Impl/NewsServiceImpl.java
@@ -1,54 +1,79 @@
 package at.fhj.swd.model.service.Impl;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
+import javax.annotation.security.PermitAll;
+
+import at.fhj.swd.model.data.NewsDao;
+import at.fhj.swd.model.data.UserDAO;
 import at.fhj.swd.model.entity.News;
 import at.fhj.swd.model.entity.User;
 import at.fhj.swd.model.service.NewsService;
 
 public class NewsServiceImpl implements NewsService{
+	
+	private NewsDao newsdao;
+	
+	private UserDAO userdao;
+	
+	private User user;
+	
+	private News news;
+	
 
 	@Override
 	public List<News> getAllNews() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.getAllNews();
 	}
 
 	@Override
-	public void postNews(String title, String content) throws IOException {
-		// TODO Auto-generated method stub
+	public void postNews(Long id, String title, String content, Date startdate, Boolean visible) throws IOException {
 		
+		if (UserIsAdmin(user) == false)
+			throw new IllegalArgumentException("Du bist kein Admin. Nur Admins dürfen NEws posten");
+			News newNews = new News();
+			newNews.setId(id);
+			newNews.setTitle(title);
+			newNews.setContent(content);
+			newNews.setStartdate(startdate);
+			newNews.setVisible(visible);
+			newsdao.createNewNews(newNews);
+			
+	}
+
+	
+
+	@Override
+	public News updateNews(News news) {
+		if (UserIsAdmin(user) || UserIsPortalAdmin(user)== false )
+			throw new IllegalArgumentException("Du bist kein Admin.");
+			if (news == null)
+				throw new IllegalArgumentException("News is null.");
+			return newsdao.updateNews(news);		
+	}
+	
+
+
+	@Override
+	@PermitAll()
+	public boolean UserIsAdmin(User user) {
+		return user.getUsername().endsWith("_a");
 	}
 
 	@Override
-	public void deleteNews(String title) {
-		// TODO Auto-generated method stub
-		
+	@PermitAll()
+	public boolean UserIsPortalAdmin(User user) {
+		return user.getUsername().endsWith("_pa");
 	}
 
 	@Override
-	public void updateNews(String name) throws IOException {
+	public List<News> getNewsbyID(Long id) {
 		// TODO Auto-generated method stub
-		
+		return this.getNewsbyID(id);
 	}
 
-	@Override
-	public boolean getAdministrationAllowed(String token) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
-	@Override
-	public User getUserByToken(String token) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean getUserAdministrationAllowed(String token) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 }
