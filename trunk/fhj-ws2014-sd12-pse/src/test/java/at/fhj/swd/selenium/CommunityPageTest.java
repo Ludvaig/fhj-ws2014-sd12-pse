31d5801fdd106bde4456699977a173c558bd5085
florian.dulzky@gmail.com <florian.dulzky@gmail.com@b4c154ee-1d16-9667-695d-569dda043578>
2014-11-28 20:40:56 +0000

    Added further seleniumtests for CommunityPage

--- trunk/fhj-ws2014-sd12-pse/src/test/java/at/fhj/swd/selenium/CommunityPageTest.java
+++ trunk/fhj-ws2014-sd12-pse/src/test/java/at/fhj/swd/selenium/CommunityPageTest.java
@@ -1,6 +1,7 @@
 package at.fhj.swd.selenium;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import at.fhj.swd.selenium.pageobjects.CommunityPage;
@@ -8,11 +9,31 @@ import at.fhj.swd.selenium.pageobjects.CommunityPage;
 
 public class CommunityPageTest extends AbstractTestSetup{
 
+	private CommunityPage communityPage;
+	
+	@Before
+	public void setup(){
+		super.setup();
+		communityPage = tmpPage.goToCommunityPage();
+	}
+	
 	@Test
 	public void openCommunityPage(){
-		CommunityPage communityPage = tmpPage.goToCommunityPage();
-		
 		String expected = "Communities";
 		Assert.assertEquals(expected, communityPage.GetTextOfxPathElement(".//*[@id='communitySearch']/h1"));
+	}
+	
+	@Test
+	public void searchCommunities() throws InterruptedException{
+		communityPage.searchForCommunity("Eor");
+		String expected = "George";
+		Assert.assertEquals(expected, communityPage.GetTextOfxPathElement(".//*[@id='communitySearch:communities_data']/tr[1]/td"));
+	}
+	
+	@Test
+	public void searchCommunitiesNotExists() throws InterruptedException{
+		communityPage.searchForCommunity("ASDF");
+		String expected = "No records found.";
+		Assert.assertEquals(expected, communityPage.GetTextOfxPathElement(".//*[@id='communitySearch:communities_data']/tr[1]/td"));
 	}
 }
