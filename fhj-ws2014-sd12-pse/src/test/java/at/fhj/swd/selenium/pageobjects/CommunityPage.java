0e5a890b77d5df121061216711cfb0bf6e6d4b3b
florian.dulzky <florian.dulzky@gmail.com>
2014-11-28 20:40:56 +0000

    Added further seleniumtests for CommunityPage

--- fhj-ws2014-sd12-pse/src/test/java/at/fhj/swd/selenium/pageobjects/CommunityPage.java
+++ fhj-ws2014-sd12-pse/src/test/java/at/fhj/swd/selenium/pageobjects/CommunityPage.java
@@ -1,5 +1,6 @@
 package at.fhj.swd.selenium.pageobjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CommunityPage extends PageObjectBase{
@@ -8,6 +9,13 @@ public class CommunityPage extends PageObjectBase{
 		super(driver);
 	}
 	
-	
+	public void searchForCommunity(String text) throws InterruptedException {
+		driver.findElement(By.xpath(".//*[@id='communitySearch:j_idt12']")).clear();
+		driver.findElement(By.xpath(".//*[@id='communitySearch:j_idt12']")).sendKeys(text);
+		driver.findElement(By.xpath(".//*[@id='communitySearch:communitySearch']")).click();
+		
+		// Wait 1 second until the ajax call is finished
+		Thread.sleep(1000);
+	}
 	
 }
