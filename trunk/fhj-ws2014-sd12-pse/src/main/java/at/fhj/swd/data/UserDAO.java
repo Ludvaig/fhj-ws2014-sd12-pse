db26729c3f280e7d02a181e63989dc3ccae9cb81
sysprog77@gmail.com <sysprog77@gmail.com@b4c154ee-1d16-9667-695d-569dda043578>
2015-01-03 19:52:40 +0000

    bugfix: Reverted inconsistent refactoring of UserDao-methods by Jörg&Steve!

--- trunk/fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/data/UserDAO.java
+++ trunk/fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/data/UserDAO.java
@@ -3,9 +3,10 @@ package at.fhj.swd.data;
 import at.fhj.swd.data.entity.User;
 
 public interface UserDAO {
-	User update(User user);
-	void insert(User user);
-	User findByName(String username);
-	User findById(long id);
-	User findByToken(String token);
-}
+		User proveUserPassswordCombination(String username, String password);
+		void persist(User user);
+		User loadUserByName(String username);
+		User loadUserById(long id); 
+		User loadUserByToken(String token); 
+		User updateUser(User user);
+	}
