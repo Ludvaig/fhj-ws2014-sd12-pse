573e94772ca32ac061b6b7fa842e71e2745c7d66
s.hagelmueller <s.hagelmueller@gmail.com>
2015-01-09 17:46:49 +0000

    Added LoggerStube for Unit testing.

---
+++ fhj-ws2014-sd12-pse/src/test/java/at/fhj/swd/service/mock/LoggerStub.java
@@ -0,0 +1,24 @@
+package at.fhj.swd.service.mock;
+
+public class LoggerStub extends java.util.logging.Logger {
+
+	protected LoggerStub(String name, String resourceBundleName) {
+		super(name, resourceBundleName);
+	}
+	
+	public LoggerStub(){
+		this(null,null);
+	}
+	
+	@Override
+	public void info(String msg) {
+	}
+	
+	@Override
+	public void warning(String msg) {
+	}
+	
+	@Override
+	public void severe(String msg) {
+	}	
+}
