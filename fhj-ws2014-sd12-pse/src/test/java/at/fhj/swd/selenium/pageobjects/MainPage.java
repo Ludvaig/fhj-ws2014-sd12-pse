431746c192e6775aa7fa27af240a5407fded1cc0
haberl.george <haberl.george@gmail.com>
2015-01-10 08:41:18 +0000

    added selenium test object for index.jsf (empty - functionality will be added later)

---
+++ fhj-ws2014-sd12-pse/src/test/java/at/fhj/swd/selenium/pageobjects/MainPage.java
@@ -0,0 +1,16 @@
+package at.fhj.swd.selenium.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Page object of index.jsf for testing.
+ * @author Haberl
+ *
+ */
+public class MainPage extends PageObjectBase {
+
+	public MainPage(WebDriver driver) {
+		super(driver);
+	}
+
+}
