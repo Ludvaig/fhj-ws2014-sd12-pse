5ac54b120c81f26221fdb523e0076a0cea6dd7b2
Thomas.Ascher.SWD12 <Thomas.Ascher.SWD12@gmail.com>
2015-01-10 09:23:23 +0000

    refactoring: DocumentServiceImpl to EJB

--- fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/service/impl/DocumentServiceImpl.java
+++ fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/service/impl/DocumentServiceImpl.java
@@ -10,8 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Stateless;
-import javax.faces.bean.ApplicationScoped;
-import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
 
 import at.fhj.swd.data.entity.Document;
@@ -26,8 +24,7 @@ import at.fhj.swd.service.UserService;
  * 
  * @author Group1
  * */
-@ManagedBean(name = "documentService")
-@ApplicationScoped
+@Stateless
 public class DocumentServiceImpl implements DocumentService {
 	
 	private static String globalContext = "global";
