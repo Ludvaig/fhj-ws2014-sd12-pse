fab01cf1f11b198abb66aeea0d88c3b17402faab
totbojan <totbojan@b4c154ee-1d16-9667-695d-569dda043578>
2015-01-11 16:45:04 +0000

    added logging

--- trunk/fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/service/impl/NewsServiceImpl.java
+++ trunk/fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/service/impl/NewsServiceImpl.java
@@ -2,9 +2,13 @@ package at.fhj.swd.service.impl;
 
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
 
 import at.fhj.swd.data.NewsDAO;
 import at.fhj.swd.data.entity.News;
@@ -17,18 +21,22 @@ public class NewsServiceImpl implements NewsService {
 	
 	@Inject
 	private NewsDAO newsDao;	
+	private static final Logger logger = Logger.getLogger(NewsServiceImpl.class.getName());
 
 	@Override
 	public List<News> getAllNews() {
+		logger.entering(getClass().getName(), "getAllNews");
 		try {
 			return newsDao.getAllNews();
 		} catch(DataSourceLayerException e) {
-			throw new ServiceLayerException("service failed to get all news", e);
+			logger.log(Level.SEVERE, "Service failed to fetch all news");
+			throw new ServiceLayerException("Service failed to fetch all news", e);		
 		}
 	}
 
 	@Override
 	public void postNews(Long id, String title, String content, Date startdate, Boolean visible) {
+		logger.entering(getClass().getName(), "postNews");
 		try {
 			News newNews = new News();
 			newNews.setTitle(title);
@@ -37,28 +45,33 @@ public class NewsServiceImpl implements NewsService {
 			newNews.setVisible(visible);
 			newsDao.createNewNews(newNews);	
 		} catch(DataSourceLayerException e) {
-			throw new ServiceLayerException("service failed to post news", e);
+			logger.log(Level.SEVERE, "Service failed to post news");
+			throw new ServiceLayerException("Service failed to post news", e);
 		}
 	}
 
 	@Override
 	public News updateNews(News news) {
+		logger.entering(getClass().getName(), "updateNews");
 		if (news == null)
 			throw new IllegalArgumentException("news");
-		
+			logger.log(Level.SEVERE, "User provided illegal argument");
 		try {
 			return newsDao.updateNews(news);
 		} catch(DataSourceLayerException e) {
-			throw new ServiceLayerException("service failed to update news", e);
+			logger.log(Level.SEVERE, "Service failed to update news");
+			throw new ServiceLayerException("Service failed to update news", e);
 		}
 	}
 
 	@Override
 	public News findNewsById(long id) {
+		logger.entering(getClass().getName(), "findNewsById");
 		try {
 			return newsDao.getNewsById(id);			
 		} catch(DataSourceLayerException e) {
-			throw new ServiceLayerException("service failed to find news with id " + id, e);
+			logger.log(Level.SEVERE, "Service failed to find news with id" + id);
+			throw new ServiceLayerException("Service failed to find news with id " + id, e);
 		}
 	}
 }
