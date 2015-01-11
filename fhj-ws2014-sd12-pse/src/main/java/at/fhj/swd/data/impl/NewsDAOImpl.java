bbad24de75ea78d8839301dba16432241f4f311e
mmayer.sd <mmayer.sd@gmail.com>
2015-01-11 12:02:40 +0000

    Refactoring group 3 NewsService incl. NewsDAO (Exceptions)

--- fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/data/impl/NewsDAOImpl.java
+++ fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/data/impl/NewsDAOImpl.java
@@ -1,7 +1,7 @@
 package at.fhj.swd.data.impl;
 
 /**
- * Gruppe 3:
+ * Group 3:
  * 
  * Update Julia: UpdateUser-Methode
  */
@@ -13,6 +13,7 @@ import javax.persistence.EntityManager;
 
 import at.fhj.swd.data.NewsDAO;
 import at.fhj.swd.data.entity.News;
+import at.fhj.swd.data.exceptions.DataSourceLayerException;
 
 public class NewsDAOImpl implements NewsDAO {
 
@@ -23,46 +24,69 @@ public class NewsDAOImpl implements NewsDAO {
 	public List<News> getAllVisibleNews() {
 		
 		// Load all visible news from database (Pagination @see: LazyCommunityImpl)
-		List<News> news = em
-				.createQuery(
-						"SELECT news FROM News news"
-						+ " WHERE news.visible = true "
-						+ " ORDER BY news.startdate",
-						News.class)
-							.getResultList();
-		
-		return news;
+		try {
+			List<News> news = em
+					.createQuery(
+							"SELECT news FROM News news"
+							+ " WHERE news.visible = true "
+							+ " ORDER BY news.startdate",
+							News.class)
+								.getResultList();
+			
+			return news;	
+		} catch (Exception e) {
+			throw new DataSourceLayerException("failed to get all visible news", e);
+		}
 	}
 	
 	@Override
 	public List<News> getAllNews() {
 		// Load all news from database (Pagination @see: LazyCommunityImpl)
-		List<News> news = em
-				.createQuery(
-					"SELECT news FROM News news"
-					+ " ORDER BY news.startdate",
-					News.class)
-				.getResultList();		
-		return news;
+		try {
+			List<News> news = em
+					.createQuery(
+						"SELECT news FROM News news"
+						+ " ORDER BY news.startdate",
+						News.class)
+					.getResultList();		
+			return news;
+		} catch (Exception e) {
+			throw new DataSourceLayerException("failed to get all news", e);
+		}
 	}
 
 	@Override
 	public News getNewsById(long id) {
-		return em.createQuery("FROM News news WHERE news.id = :id", News.class)
-				.setParameter("id", Long.valueOf(id))
-				.getSingleResult();
+		try {
+			return em.createQuery("FROM News news WHERE news.id = :id", News.class)
+					.setParameter("id", Long.valueOf(id))
+					.getSingleResult();
+		} catch (Exception e) {
+			throw new DataSourceLayerException("failed to get news by id", e);
+		}
 	}
 	
 	@Override
 	public void createNewNews(News newNews) {
-		em.persist(newNews);
+		if(newNews == null) {
+			throw new IllegalArgumentException("news");
+		}	
+		try {
+			em.persist(newNews);
+		} catch (Exception e) {
+			throw new DataSourceLayerException("failed to create news", e);
+		}		
 	}
 
 	@Override
 	public News updateNews(News news) {
-		if (news != null)
+		if(news == null) {
+			throw new IllegalArgumentException("news");
+		}		
+		try {
 			return em.merge(news);
-		else
-			return null;		
+		} catch (Exception e) {
+			throw new DataSourceLayerException("failed to get all visible news", e);
+		}		
 	}
 }
