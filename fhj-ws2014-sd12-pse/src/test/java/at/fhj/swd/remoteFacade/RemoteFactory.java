a9f295a6ae857c23f8c33f3a3f5d67afd29006c9
Thomas.Ascher.SWD12 <Thomas.Ascher.SWD12@gmail.com>
2015-01-11 14:33:40 +0000

    refactoring: moved remote object creation into RemoteFactory class

---
+++ fhj-ws2014-sd12-pse/src/test/java/at/fhj/swd/remoteFacade/RemoteFactory.java
@@ -0,0 +1,54 @@
+package at.fhj.swd.remoteFacade;
+
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+// The JNDI lookup name for a stateless session bean has the syntax of:
+// ejb:<appName>/<moduleName>/<distinctName>/<beanName>!<viewClassName>
+//
+// <appName> The application name is the name of the EAR that the EJB is
+// deployed in
+// (without the .ear). If the EJB JAR is not deployed in an EAR then this is
+// blank. The app name can also be specified in the EAR's application.xml
+//
+// <moduleName> By the default the module name is the name of the EJB JAR
+// file (without the
+// .jar suffix). The module name might be overridden in the ejb-jar.xml
+//
+// <distinctName> : WildFly allows each deployment to have an (optional)
+// distinct name.
+// This example does not use this so leave it blank.
+//
+// <beanName> : The name of the session been to be invoked.
+//
+// <viewClassName>: The fully qualified classname of the remote interface.
+// Must include
+// the whole package name.
+
+// "ejb:/wildfly-ejb-remote-server-side/CalculatorBean!" +
+// RemoteCalculator.class.getName()
+// java:global/EJB-Calculator-RemoteTest/CalculatorTestBean!org.se.lab.Calculator
+// ejb:/EJB-Calculator-RemoteTest/CalculatorTestBean!org.se.lab.Calculator
+
+/**
+ * Instantiates remote object on JBoss server
+ * @author Thomas Ascher
+ *
+ */
+public class RemoteFactory {
+
+	public static Object create(final String beanName, final String interfaceName) throws NamingException {
+		Context context = createContext();
+		final String jndiName = "ejb:/fhj-ws2014-sd12-pse//" + beanName + "!" + interfaceName;
+		return context.lookup(jndiName);			
+	}
+	
+	private static Context createContext() throws NamingException {
+		final Hashtable<String, String> jndiProperties = new Hashtable<String, String>();
+		jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+		return new InitialContext(jndiProperties);
+	}
+}
--- fhj-ws2014-sd12-pse/src/test/java/at/fhj/swd/remoteFacade/UserServiceRemoteFacadeTest.java
+++ fhj-ws2014-sd12-pse/src/test/java/at/fhj/swd/remoteFacade/UserServiceRemoteFacadeTest.java
@@ -3,22 +3,19 @@ package at.fhj.swd.remoteFacade;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import java.util.Hashtable;
 import java.util.Random;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import at.fhj.swd.data.entity.User;
-import at.fhj.swd.domain.util.HashUtil;
 import at.fhj.swd.service.UserService;
 import at.fhj.swd.service.exceptions.UserLoginException;
+
+import at.fhj.swd.remoteFacade.RemoteFactory;
 
 public final class UserServiceRemoteFacadeTest {
 
@@ -28,45 +25,9 @@ public final class UserServiceRemoteFacadeTest {
 
 	private UserService service;
 
-	// The JNDI lookup name for a stateless session bean has the syntax of:
-	// ejb:<appName>/<moduleName>/<distinctName>/<beanName>!<viewClassName>
-	//
-	// <appName> The application name is the name of the EAR that the EJB is
-	// deployed in
-	// (without the .ear). If the EJB JAR is not deployed in an EAR then this is
-	// blank. The app name can also be specified in the EAR's application.xml
-	//
-	// <moduleName> By the default the module name is the name of the EJB JAR
-	// file (without the
-	// .jar suffix). The module name might be overridden in the ejb-jar.xml
-	//
-	// <distinctName> : WildFly allows each deployment to have an (optional)
-	// distinct name.
-	// This example does not use this so leave it blank.
-	//
-	// <beanName> : The name of the session been to be invoked.
-	//
-	// <viewClassName>: The fully qualified classname of the remote interface.
-	// Must include
-	// the whole package name.
-
-	// "ejb:/wildfly-ejb-remote-server-side/CalculatorBean!" +
-	// RemoteCalculator.class.getName()
-	// java:global/EJB-Calculator-RemoteTest/CalculatorTestBean!org.se.lab.Calculator
-	// ejb:/EJB-Calculator-RemoteTest/CalculatorTestBean!org.se.lab.Calculator
-
 	@Before
 	public void setUp() throws NamingException {
-		final Hashtable<String, String> jndiProperties = new Hashtable<String, String>();
-		jndiProperties.put(Context.URL_PKG_PREFIXES,
-				"org.jboss.ejb.client.naming");
-		final Context context = new InitialContext(jndiProperties);
-
-		final String jndiName = "ejb:" + "" + "/" + "fhj-ws2014-sd12-pse" + "/"
-				+ "" + "/" + "UserServiceRemoteFacade" + "!"
-				+ UserService.class.getName();
-
-		service = (UserService) context.lookup(jndiName);
+		service = (UserService)RemoteFactory.create("UserServiceRemoteFacade", UserService.class.getName());
 
 		user = new User();
 		user.setUsername(userName);
