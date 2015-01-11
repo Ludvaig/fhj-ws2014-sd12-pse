c15c8605f50adcbf183b7c12a5b5b7b6bf2b31a6
Thomas.Ascher.SWD12@gmail.com <Thomas.Ascher.SWD12@gmail.com@b4c154ee-1d16-9667-695d-569dda043578>
2015-01-11 12:15:56 +0000

    allow to open news in own dialog

--- trunk/fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/presentation/viewHelper/NewsView.java
+++ trunk/fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/presentation/viewHelper/NewsView.java
@@ -1,6 +1,5 @@
 package at.fhj.swd.presentation.viewHelper;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,15 +8,11 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
-import org.primefaces.event.SelectEvent;
-
 import at.fhj.swd.data.entity.News;
-import at.fhj.swd.presentation.helper.CookieHelper;
 import at.fhj.swd.service.NewsService;
 
 /**
@@ -42,22 +37,22 @@ public class NewsView implements Serializable{
 	@EJB
     private NewsService service;
     
-    
     @PostConstruct
     public void init() {
     	try {
-    		allNews = service.getAllNews();
-			
+    		allNews = service.getAllNews();    		
+    		if (allNews.size() != 0) {
+    			selectedNews = allNews.get(0);
+    		}
 		} catch (Exception e) {
 			allNews = new ArrayList<News>();
 			String errorMessage = e.getLocalizedMessage();
 			facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, ""));
 		}
-    	//service.ensureUserIsLoggedIn();
     }
     
     public List<News> getAllNews() {
-    	return allNews;
+    	return service.getAllNews();
     }
     
     public void setService(NewsService service) {
@@ -71,12 +66,4 @@ public class NewsView implements Serializable{
 	public void setSelectedNews(News selectedNews) {
 		this.selectedNews = selectedNews;
 	}
-    
-    public void onNewsSelected(SelectEvent object){
-		try {
-			FacesContext.getCurrentInstance().getExternalContext().redirect("news_detail.jsf?id=" + selectedNews.getId());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
 }
