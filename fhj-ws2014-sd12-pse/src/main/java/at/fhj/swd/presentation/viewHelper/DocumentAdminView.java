53b12c05a093c02a817c1db3b2bcbe22dfbbe0c3
kamper.manuel <kamper.manuel@gmail.com>
2015-01-12 15:40:21 +0000

    presentation/viewHelper/NewsView.java 	- added logging presentation/viewHelper/DocumentView.java 	- added logging presentation/viewHelper/DocumentAdminView.java 	- added logging

--- fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/presentation/viewHelper/DocumentAdminView.java
+++ fhj-ws2014-sd12-pse/src/main/java/at/fhj/swd/presentation/viewHelper/DocumentAdminView.java
@@ -2,9 +2,12 @@ package at.fhj.swd.presentation.viewHelper;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.DefaultStreamedContent;
@@ -16,7 +19,7 @@ import at.fhj.swd.presentation.helper.CookieHelper;
 /**
  * Document/Admin-ViewHelper.
  * 
- * @author Group3
+ * @author Group3, Kamper
  * */
 
 @ManagedBean(name="dtDocumentAdminView")
@@ -27,15 +30,18 @@ public class DocumentAdminView extends DocumentView {
     }
 	
 	public void handleFileUpload(FileUploadEvent event) throws IOException {
+		logger.log(Level.INFO, "Uploading Doc " + event.getFile().getFileName() + " via Admin into Documents.");
     	this.service.uploadAdminDocument(event.getFile().getInputstream(), event.getFile().getFileName());
     }
 
     public void setDeleteDocument(String name) {
+		logger.log(Level.INFO, "Deleting Doc " + name + " from Documents via Admin.");
     	this.service.deleteAdminDocument(name);
     }
     
 	public DefaultStreamedContent getDownload() throws IOException {
     	String name = this.getSelectedDocument();
+    	logger.log(Level.INFO, "Downloading Doc " + name + " from Documents.");
     	return new DefaultStreamedContent(this.service.downloadAdminDocument(name), null, name);
     }
 	
