e9ccc63e1f564ff780243e261921ff6f3e566236
tschuliiia@gmail.com <tschuliiia@gmail.com@b4c154ee-1d16-9667-695d-569dda043578>
2015-01-12 16:22:03 +0000

    newspagetest committen versuchn

---
+++ trunk/fhj-ws2014-sd12-pse/src/test/java/at/fhj/swd/selenium/NewsPageTest.java
@@ -0,0 +1,65 @@
+package at.fhj.swd.selenium;
+
+import static org.junit.Assert.assertTrue;
+
+/** author: Julia Viehberger
+ * 
+ */
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import at.fhj.swd.selenium.pageobjects.AdminPage;
+
+
+public class NewsPageTest extends AbstractTestSetup{
+
+	private AdminPage adminPage;
+	String expected;
+	
+	@Before
+	public void setup(){
+		super.setup();
+
+	}
+	
+	@Test
+	public void tryToCreateNewsAsAdmin(){
+		String newNewsTitle = "NewNews";
+		String newNewsContent = "NewNewsContent";	
+		
+		loginAdmin();
+		adminPage = tmpPage.goToAdminPage();
+		adminPage = tmpPage.goToAdminNewsPage();
+		adminPage = adminPage.createNews(newNewsTitle, newNewsContent);
+		
+		assertTrue(driver.getPageSource().contains(newNewsTitle));
+
+	}
+	@Test
+	public void openAdminNewsPage(){
+		loginAdmin();
+		adminPage = tmpPage.goToAdminPage();
+
+		adminPage = tmpPage.goToAdminNewsPage();
+		String expected = "News";
+		Assert.assertEquals(expected, adminPage.GetTextOfxPathElement(".//*[@id='newstitle']/h1"));
+	}
+	
+	@Test
+	public void tryToEditNewsAsAdmin() {
+		String newNewsTitle = "NewsTitleChanged!!";
+		String newNewsContent = "NewsContentChanged";	
+		
+		loginAdmin();
+		adminPage = tmpPage.goToAdminPage();
+		adminPage = tmpPage.goToAdminNewsPage();
+		adminPage = adminPage.editNews(newNewsTitle, newNewsContent);
+		
+		assertTrue(driver.getPageSource().contains(newNewsTitle));
+	}
+	
+
+
+}
